123fb26ed3a86003773fd05fa7233b904efcd6b5
Blockchain Library <33207942+blockchainlibraryorg@users.noreply.github.com>
2018-05-24 14:53:39 -0400

    Add SmartBee
    from Flauwy

---
+++ Video/SmartBee/ACA Projects/SmartBee Intro/Ch Data/temp.noindex/compiler/bootstrap.js
@@ -0,0 +1,3 @@
+var __bootStrapPath = "D:\\Video Productions\\SmartBee Intro\\Ch Data\\temp.noindex\\compiler\\bootstrap.js";
+var __compiledScene = "D:\\Video Productions\\SmartBee Intro\\Ch Data\\temp.noindex\\compiler\\scene.json";
+runScript("C:\\Program Files\\Adobe\\Adobe Character Animator CC 2018\\Support Files\\Runtime\\animal\\scripts\\js\\lib\\engine.js")
---
+++ Video/SmartBee/ACA Projects/SmartBee Intro/Ch Data/temp12692.noindex/compiler/bootstrap.js
@@ -0,0 +1,3 @@
+var __bootStrapPath = "D:\\Video Productions\\SmartBee Intro\\Ch Data\\temp12692.noindex\\compiler\\bootstrap.js";
+var __compiledScene = "D:\\Video Productions\\SmartBee Intro\\Ch Data\\temp12692.noindex\\compiler\\scene.json";
+runScript("C:\\Program Files\\Adobe\\Adobe Character Animator CC 2018\\Support Files\\Runtime\\animal\\scripts\\js\\lib\\engine.js")
